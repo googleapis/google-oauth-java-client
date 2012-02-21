ab25cb18164b7d96a0b8fdd3eef65bec41154087
Yaniv Inbar <yanivi@google.com>
2012-02-21 10:42:07 -0500

    MemoryCredentialStore, JdoCredentialStore, and AppEngineCredentialStore
    http://codereview.appspot.com/5675068/

---
+++ google-oauth-client-appengine/src/main/java/com/google/api/client/extensions/appengine/auth/oauth2/AppEngineCredentialStore.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.extensions.appengine.auth.oauth2;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.CredentialStore;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+/**
+ * Thread-safe Google App Engine implementation of a credential store that directly uses the App
+ * Engine Data Store API.
+ *
+ * @since 1.7
+ * @author Yaniv Inbar
+ */
+public class AppEngineCredentialStore implements CredentialStore {
+
+  private static final String KIND = AppEngineCredentialStore.class.getName();
+
+  @Override
+  public void store(String userId, Credential credential) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity(KIND, userId);
+    entity.setProperty("accessToken", credential.getAccessToken());
+    entity.setProperty("refreshToken", credential.getRefreshToken());
+    entity.setProperty("expirationTimeMillis", credential.getExpirationTimeMilliseconds());
+    datastore.put(entity);
+  }
+
+  @Override
+  public void delete(String userId, Credential credential) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Key key = KeyFactory.createKey(KIND, userId);
+    datastore.delete(key);
+  }
+
+  @Override
+  public boolean load(String userId, Credential credential) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Key key = KeyFactory.createKey(KIND, userId);
+    try {
+      Entity entity = datastore.get(key);
+      credential.setAccessToken((String) entity.getProperty("accessToken"));
+      credential.setRefreshToken((String) entity.getProperty("refreshToken"));
+      credential.setExpirationTimeMilliseconds((Long) entity.getProperty("expirationTimeMillis"));
+      return true;
+    } catch (EntityNotFoundException exception) {
+      return false;
+    }
+  }
+}
---
+++ google-oauth-client/src/main/java/com/google/api/client/auth/oauth2/MemoryCredentialStore.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.auth.oauth2;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Thread-safe in-memory implementation of a credential store.
+ *
+ * @since 1.7
+ * @author Yaniv Inbar
+ */
+public class MemoryCredentialStore implements CredentialStore {
+
+  /** Lock on access to the store. */
+  private final Lock lock = new ReentrantLock();
+
+  /** Store of memory persisted credentials, indexed by userId. */
+  private final Map<String, MemoryPersistedCredential> store =
+      new HashMap<String, MemoryPersistedCredential>();
+
+  public void store(String userId, Credential credential) {
+    lock.lock();
+    try {
+      MemoryPersistedCredential item = store.get(userId);
+      if (item == null) {
+        item = new MemoryPersistedCredential();
+        store.put(userId, item);
+      }
+      item.store(credential);
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public void delete(String userId, Credential credential) {
+    lock.lock();
+    try {
+      store.remove(userId);
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public boolean load(String userId, Credential credential) {
+    lock.lock();
+    try {
+      MemoryPersistedCredential item = store.get(userId);
+      if (item != null) {
+        item.load(credential);
+      }
+      return item != null;
+    } finally {
+      lock.unlock();
+    }
+  }
+}
