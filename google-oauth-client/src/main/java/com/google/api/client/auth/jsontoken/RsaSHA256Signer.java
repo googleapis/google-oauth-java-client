251cd7cf8ee72a6e638e80ffb8e9cbfa7ef2b0ac
Yaniv Inbar <yanivi@google.com>
2012-02-27 14:56:08 -0500

    RsaSHA256Signer
    http://codereview.appspot.com/5699092/

---
+++ google-oauth-client/src/main/java/com/google/api/client/auth/jsontoken/RsaSHA256Signer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.auth.jsontoken;
+
+import com.google.api.client.json.JsonFactory;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
+
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.Signature;
+
+/**
+ * Signs a JSON Web Signature (JWS) using RSA and SHA-256.
+ *
+ * @since 1.7
+ * @author Yaniv Inbar
+ */
+public class RsaSHA256Signer {
+
+  /**
+   * Signs a given JWS header and payload based on the given private key.
+   *
+   * @param privateKey private key
+   * @param jsonFactory JSON factory
+   * @param header JWS header
+   * @param payload JWS payload
+   * @return signed JWS string
+   */
+  public static String sign(PrivateKey privateKey, JsonFactory jsonFactory,
+      JsonWebSignature.Header header, JsonWebToken.Payload payload)
+      throws GeneralSecurityException {
+    String content = Base64.encodeBase64URLSafeString(jsonFactory.toByteArray(header)) + "."
+        + Base64.encodeBase64URLSafeString(jsonFactory.toByteArray(payload));
+    byte[] contentBytes = StringUtils.getBytesUtf8(content);
+    Signature signer = Signature.getInstance("SHA256withRSA");
+    signer.initSign(privateKey);
+    signer.update(contentBytes);
+    byte[] signature = signer.sign();
+    return content + "." + Base64.encodeBase64URLSafeString(signature);
+  }
+
+  private RsaSHA256Signer() {
+  }
+}
---
+++ google-oauth-client/src/test/java/com/google/api/client/auth/jsontoken/RsaSHA256SignerTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.auth.jsontoken;
+
+import com.google.api.client.json.jackson.JacksonFactory;
+
+import junit.framework.TestCase;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+
+/**
+ * Tests {@link RsaSHA256Signer}.
+ *
+ * @author Yaniv Inbar
+ */
+public class RsaSHA256SignerTest extends TestCase {
+
+  public void testSign() throws GeneralSecurityException {
+    JsonWebSignature.Header header = new JsonWebSignature.Header();
+    header.setAlgorithm("RS256");
+    header.setType("JWT");
+    JsonWebToken.Payload payload = new JsonWebToken.Payload();
+    long currentTime = System.currentTimeMillis();
+    payload.setIssuer("issuer").setAudience("audience").setIssuedAtTimeSeconds(currentTime / 1000)
+        .setExpirationTimeSeconds(currentTime / 1000 + 3600);
+    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+    keyGen.initialize(512, new SecureRandom(new byte[0]));
+    PrivateKey privateKey = keyGen.generateKeyPair().getPrivate();
+    RsaSHA256Signer.sign(privateKey, new JacksonFactory(), header, payload);
+  }
+}
