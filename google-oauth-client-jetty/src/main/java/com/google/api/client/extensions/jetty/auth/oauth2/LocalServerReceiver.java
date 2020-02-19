48fb3f261562151c047a3f0fb9b1add1c152e635
eraskin <eraskin@paslists.com>
2020-02-19 16:20:52 -0500

    feat: Rewrite findOpenSocket using try with resources

--- google-oauth-client-jetty/src/main/java/com/google/api/client/extensions/jetty/auth/oauth2/LocalServerReceiver.java
+++ google-oauth-client-jetty/src/main/java/com/google/api/client/extensions/jetty/auth/oauth2/LocalServerReceiver.java
@@ -159,27 +159,12 @@ public final class LocalServerReceiver implements VerificationCodeReceiver {
    */
 
   private int findOpenPort() {
-    ServerSocket socket = null;
-    try {
-      socket = new ServerSocket(0);
+    try (ServerSocket socket = new ServerSocket(0)) {
       socket.setReuseAddress(true);
-      int port = socket.getLocalPort();
-      try {
-        socket.close();
-      } catch (IOException e) {
-        // Ignore IOException on close()
-      }
-      return port;
-    } catch (IOException e) {
-    } finally {
-      if (socket != null) {
-        try {
-          socket.close();
-        } catch (IOException e) {
-        }
-      }
+      return socket.getLocalPort();
+    } catch(IOException e) {
+      throw new IllegalStateException("No free TCP/IP port to start embedded HTTP Server on");
     }
-    throw new IllegalStateException("No free TCP/IP port to start embedded HTTP Server on");
   }
 
   /**
