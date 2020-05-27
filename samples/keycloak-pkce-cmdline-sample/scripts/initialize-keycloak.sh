73fa76042ed709fb9bd2421f81aeb16da5396794
Stefán Freyr Stefánsson <stefan@wuxinextcode.com>
2020-05-27 18:15:42 +0000

    Improve keycloak init script and some code cleanup. Still WIP.

--- samples/keycloak-pkce-cmdline-sample/scripts/initialize-keycloak.sh
+++ samples/keycloak-pkce-cmdline-sample/scripts/initialize-keycloak.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#Start keycloak server:
+# docker run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:10.0.1
+
+
 KEYCLOAK_BASE_URL="http://localhost:8080/auth"
 KEYCLOAK_REALM="master"
 KEYCLOAK_URL="${KEYCLOAK_BASE_URL}/realms/${KEYCLOAK_REALM}"
@@ -14,8 +18,10 @@ export TKN=$(curl -s -X POST "${KEYCLOAK_URL}/protocol/openid-connect/token" \
  -d 'grant_type=password' \
  -d 'client_id=admin-cli' | jq -r '.access_token')
 
+#curl -s -X GET "${KEYCLOAK_BASE_URL}/admin/realms/${KEYCLOAK_REALM}/clients" -H "Content-Type:application/json" -H "Authorization: bearer ${TKN}" |jq
+
 curl -s -X POST "${KEYCLOAK_URL}/clients-registrations/default" \
- -d '{ "clientId": "myclient", "redirectUris": ["http://localhost*"], "attributes": {"pkce.code.challenge.method": "S256"} }' \
+ -d '{ "clientId": "pkce-test-client", "publicClient": true, "redirectUris": ["http://127.0.0.1*"], "attributes": {"pkce.code.challenge.method": "S256"} }' \
  -H "Content-Type:application/json" \
  -H "Authorization: bearer ${TKN}" 
 
