0196cfabb8834396bb17360c489ab1e19112cbe5
Stefán Freyr Stefánsson <stefan.freyr@gmail.com>
2020-06-19 23:32:55 +0000

    Add license header with copyright to new files. Improve documentation.

--- samples/keycloak-pkce-cmdline-sample/scripts/initialize-keycloak.sh
+++ samples/keycloak-pkce-cmdline-sample/scripts/initialize-keycloak.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#Start keycloak server:
+
+#Start keycloak server before running this script:
 # docker run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:10.0.1
 
+# The following script will create a new public client in the running keycloak server
+# in which PKCE is required for obtaining an authorization token via the authorization
+# code flow. Once this script has been run, the PKCESample.java sample application can
+# be run.
 
 KEYCLOAK_BASE_URL="http://localhost:8080/auth"
 KEYCLOAK_REALM="master"
@@ -17,8 +35,6 @@ export TKN=$(curl -s -X POST "${KEYCLOAK_URL}/protocol/openid-connect/token" \
  -d "password=${KEYCLOAK_CLIENT_SECRET}" \
  -d 'grant_type=password' \
  -d 'client_id=admin-cli' | jq -r '.access_token')
-
-#curl -s -X GET "${KEYCLOAK_BASE_URL}/admin/realms/${KEYCLOAK_REALM}/clients" -H "Content-Type:application/json" -H "Authorization: bearer ${TKN}" |jq
 
 curl -s -X POST "${KEYCLOAK_URL}/clients-registrations/default" \
  -d '{ "clientId": "pkce-test-client", "publicClient": true, "redirectUris": ["http://127.0.0.1*"], "attributes": {"pkce.code.challenge.method": "S256"} }' \
--- samples/keycloak-pkce-cmdline-sample/src/main/java/com/google/api/services/samples/keycloak/cmdline/PKCESample.java
+++ samples/keycloak-pkce-cmdline-sample/src/main/java/com/google/api/services/samples/keycloak/cmdline/PKCESample.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.api.services.samples.keycloak.cmdline;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
@@ -17,6 +31,18 @@ import com.google.api.client.util.store.MemoryDataStoreFactory;
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * A sample application that demonstrates how the Google OAuth2 library can be used to authenticate
+ * against a locally running Keycloak server with a registered public client where using
+ * <a href="https://tools.ietf.org/html/rfc7636">PKCE</a> is required.
+ *
+ * Please note that before running this sample application, a local Keycloak server must be running
+ * and a PKCE enabled client must have been defined. Please see
+ * <code>samples/keycloak-pkce-cmdline-sample/scripts/initialize-keycloak.sh</code> for further
+ * information.
+ *
+ * @author Stefan Freyr Stefansson
+ */
 public class PKCESample {
     /**
      * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
