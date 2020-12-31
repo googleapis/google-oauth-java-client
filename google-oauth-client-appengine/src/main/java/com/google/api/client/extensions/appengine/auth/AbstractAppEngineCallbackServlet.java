cf4bd00251414061b1b133dfec2b5f306e70c54a
Elliotte Rusty Harold <elharo@google.com>
2020-12-31 06:42:01 -0500

    format

--- google-oauth-client-appengine/src/main/java/com/google/api/client/extensions/appengine/auth/AbstractAppEngineCallbackServlet.java
+++ google-oauth-client-appengine/src/main/java/com/google/api/client/extensions/appengine/auth/AbstractAppEngineCallbackServlet.java
@@ -49,8 +49,8 @@ import com.google.api.client.util.Beta;
  * com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeCallbackServlet}.
  *
  * <p>Upgrade warning: in version 1.15 there was an implementation of {@link
- * #newJsonFactoryInstance()} that used {@code com.google.api.client.json.jackson.GsonFactory},
- * but starting with version 1.16 there is no such implementation.
+ * #newJsonFactoryInstance()} that used {@code com.google.api.client.json.jackson.GsonFactory}, but
+ * starting with version 1.16 there is no such implementation.
  *
  * @author moshenko@google.com (Jacob Moshenko)
  * @since 1.4
