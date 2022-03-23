acb985f4323c6e6e4cf8e6a176018aa803c369ca
Timur Sadykov <stim@google.com>
2022-03-23 03:16:42 -0700

    verifier signature fix port

--- google-oauth-client/src/main/java/com/google/api/client/auth/openidconnect/IdTokenVerifier.java
+++ google-oauth-client/src/main/java/com/google/api/client/auth/openidconnect/IdTokenVerifier.java
@@ -14,11 +14,51 @@
 
 package com.google.api.client.auth.openidconnect;
 
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.webtoken.JsonWebSignature;
+import com.google.api.client.json.webtoken.JsonWebSignature.Header;
+import com.google.api.client.util.Base64;
 import com.google.api.client.util.Beta;
 import com.google.api.client.util.Clock;
+import com.google.api.client.util.Key;
 import com.google.api.client.util.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
+import java.security.spec.ECPublicKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * {@link Beta} <br>
@@ -48,12 +88,18 @@ import java.util.Collections;
  */
 @Beta
 public class IdTokenVerifier {
+  private static final String IAP_CERT_URL = "https://www.gstatic.com/iap/verify/public_key-jwk";
+  private static final String FEDERATED_SIGNON_CERT_URL =
+      "https://www.googleapis.com/oauth2/v3/certs";
+  private static final Set<String> SUPPORTED_ALGORITHMS = ImmutableSet.of("RS256", "ES256");
 
   /** Default value for seconds of time skew to accept when verifying time (5 minutes). */
   public static final long DEFAULT_TIME_SKEW_SECONDS = 300;
 
   /** Clock to use for expiration checks. */
   private final Clock clock;
+  private final String certificatesLocation;
+  private final LoadingCache<String, Map<String, PublicKey>> publicKeyCache;
 
   /** Seconds of time skew to accept when verifying time. */
   private final long acceptableTimeSkewSeconds;
@@ -76,11 +122,16 @@ public class IdTokenVerifier {
 
   /** @param builder builder */
   protected IdTokenVerifier(Builder builder) {
+    this.certificatesLocation = builder.certificatesLocation;
     clock = builder.clock;
     acceptableTimeSkewSeconds = builder.acceptableTimeSkewSeconds;
     issuers = builder.issuers == null ? null : Collections.unmodifiableCollection(builder.issuers);
     audience =
         builder.audience == null ? null : Collections.unmodifiableCollection(builder.audience);
+    this.publicKeyCache =
+        CacheBuilder.newBuilder()
+            .expireAfterWrite(1, TimeUnit.HOURS)
+            .build(new PublicKeyLoader(builder.httpTransportFactory));
   }
 
   /** Returns the clock. */
@@ -141,10 +192,52 @@ public class IdTokenVerifier {
    * @param idToken ID token
    * @return {@code true} if verified successfully or {@code false} if failed
    */
-  public boolean verify(IdToken idToken) {
-    return (issuers == null || idToken.verifyIssuer(issuers))
+  public boolean verify(IdToken idToken) throws VerificationException {
+    boolean simpleChecks = (issuers == null || idToken.verifyIssuer(issuers))
         && (audience == null || idToken.verifyAudience(audience))
         && idToken.verifyTime(clock.currentTimeMillis(), acceptableTimeSkewSeconds);
+
+    if (!simpleChecks) {
+      return false;
+    }
+
+    PublicKey publicKeyToUse = null;
+    try {
+      String certificateLocation = getCertificateLocation(idToken.getHeader());
+      publicKeyToUse =
+          publicKeyCache.get(certificateLocation).get(idToken.getHeader().getKeyId());
+    } catch (ExecutionException | UncheckedExecutionException e) {
+      throw new VerificationException("Error fetching PublicKey from certificate location", e);
+    }
+
+    if (publicKeyToUse == null) {
+      throw new VerificationException(
+          "Could not find PublicKey for provided keyId: "
+              + idToken.getHeader().getKeyId());
+    }
+
+    try {
+      if (idToken.verifySignature(publicKeyToUse)) {
+        return true;
+      }
+      throw new VerificationException("Invalid signature");
+    } catch (GeneralSecurityException e) {
+      throw new VerificationException("Error validating token", e);
+    }
+  }
+
+  private String getCertificateLocation(Header header)
+      throws VerificationException {
+    if (certificatesLocation != null) return certificatesLocation;
+
+    switch (header.getAlgorithm()) {
+      case "RS256":
+        return FEDERATED_SIGNON_CERT_URL;
+      case "ES256":
+        return IAP_CERT_URL;
+    }
+
+    throw new VerificationException("Unknown algorithm");
   }
 
   /**
@@ -161,6 +254,8 @@ public class IdTokenVerifier {
     /** Clock. */
     Clock clock = Clock.SYSTEM;
 
+    String certificatesLocation;
+
     /** Seconds of time skew to accept when verifying time. */
     long acceptableTimeSkewSeconds = DEFAULT_TIME_SKEW_SECONDS;
 
@@ -169,6 +264,7 @@ public class IdTokenVerifier {
 
     /** List of trusted audience client IDs or {@code null} to suppress the audience check. */
     Collection<String> audience;
+    HttpTransportFactory httpTransportFactory;
 
     /** Builds a new instance of {@link IdTokenVerifier}. */
     public IdTokenVerifier build() {
@@ -214,6 +310,18 @@ public class IdTokenVerifier {
       } else {
         return setIssuers(Collections.singleton(issuer));
       }
+    }
+
+    /**
+     * Override the location URL that contains published public keys. Defaults to well-known Google
+     * locations.
+     *
+     * @param certificatesLocation URL to published public keys
+     * @return the builder
+     */
+    public Builder setCertificatesLocation(String certificatesLocation) {
+      this.certificatesLocation = certificatesLocation;
+      return this;
     }
 
     /**
@@ -279,6 +387,154 @@ public class IdTokenVerifier {
       Preconditions.checkArgument(acceptableTimeSkewSeconds >= 0);
       this.acceptableTimeSkewSeconds = acceptableTimeSkewSeconds;
       return this;
+    }
+
+    /**
+     * Set the HttpTransportFactory used for requesting public keys from the certificate URL. Used
+     * mostly for testing.
+     *
+     * @param httpTransportFactory the HttpTransportFactory used to build certificate URL requests
+     * @return the builder
+     */
+    public Builder setHttpTransportFactory(HttpTransportFactory httpTransportFactory) {
+      this.httpTransportFactory = httpTransportFactory;
+      return this;
+    }
+  }
+
+  /** Custom CacheLoader for mapping certificate urls to the contained public keys. */
+  static class PublicKeyLoader extends CacheLoader<String, Map<String, PublicKey>> {
+    private final HttpTransportFactory httpTransportFactory;
+
+    /**
+     * Data class used for deserializing a JSON Web Key Set (JWKS) from an external HTTP request.
+     */
+    public static class JsonWebKeySet extends GenericJson {
+      @Key
+      public List<JsonWebKey> keys;
+    }
+
+    /** Data class used for deserializing a single JSON Web Key. */
+    public static class JsonWebKey {
+      @Key public String alg;
+
+      @Key public String crv;
+
+      @Key public String kid;
+
+      @Key public String kty;
+
+      @Key public String use;
+
+      @Key public String x;
+
+      @Key public String y;
+
+      @Key public String e;
+
+      @Key public String n;
+    }
+
+    PublicKeyLoader(HttpTransportFactory httpTransportFactory) {
+      super();
+      this.httpTransportFactory = httpTransportFactory;
+    }
+
+    @Override
+    public Map<String, PublicKey> load(String certificateUrl) throws Exception {
+      HttpTransport httpTransport = new NetHttpTransport();
+      JsonWebKeySet jwks;
+      try {
+        HttpRequest request =
+            httpTransport
+                .createRequestFactory()
+                .buildGetRequest(new GenericUrl(certificateUrl))
+                .setParser(GsonFactory.getDefaultInstance().createJsonObjectParser());
+        HttpResponse response = request.execute();
+        jwks = response.parseAs(JsonWebKeySet.class);
+      } catch (IOException io) {
+        return ImmutableMap.of();
+      }
+
+      ImmutableMap.Builder<String, PublicKey> keyCacheBuilder = new ImmutableMap.Builder<>();
+      if (jwks.keys == null) {
+        // Fall back to x509 formatted specification
+        for (String keyId : jwks.keySet()) {
+          String publicKeyPem = (String) jwks.get(keyId);
+          keyCacheBuilder.put(keyId, buildPublicKey(publicKeyPem));
+        }
+      } else {
+        for (JsonWebKey key : jwks.keys) {
+          try {
+            keyCacheBuilder.put(key.kid, buildPublicKey(key));
+          } catch (NoSuchAlgorithmException
+              | InvalidKeySpecException
+              | InvalidParameterSpecException ignored) {
+            ignored.printStackTrace();
+          }
+        }
+      }
+
+      return keyCacheBuilder.build();
+    }
+
+    private PublicKey buildPublicKey(JsonWebKey key)
+        throws NoSuchAlgorithmException, InvalidParameterSpecException, InvalidKeySpecException {
+      if ("ES256".equals(key.alg)) {
+        return buildEs256PublicKey(key);
+      } else if ("RS256".equals((key.alg))) {
+        return buildRs256PublicKey(key);
+      } else {
+        return null;
+      }
+    }
+
+    private PublicKey buildPublicKey(String publicPem)
+        throws CertificateException, UnsupportedEncodingException {
+      return CertificateFactory.getInstance("X.509")
+          .generateCertificate(new ByteArrayInputStream(publicPem.getBytes("UTF-8")))
+          .getPublicKey();
+    }
+
+    private PublicKey buildRs256PublicKey(JsonWebKey key)
+        throws NoSuchAlgorithmException, InvalidKeySpecException {
+      com.google.common.base.Preconditions.checkArgument("RSA".equals(key.kty));
+      com.google.common.base.Preconditions.checkNotNull(key.e);
+      com.google.common.base.Preconditions.checkNotNull(key.n);
+
+      BigInteger modulus = new BigInteger(1, Base64.decodeBase64(key.n));
+      BigInteger exponent = new BigInteger(1, Base64.decodeBase64(key.e));
+
+      RSAPublicKeySpec spec = new RSAPublicKeySpec(modulus, exponent);
+      KeyFactory factory = KeyFactory.getInstance("RSA");
+      return factory.generatePublic(spec);
+    }
+
+    private PublicKey buildEs256PublicKey(JsonWebKey key)
+        throws NoSuchAlgorithmException, InvalidParameterSpecException, InvalidKeySpecException {
+      com.google.common.base.Preconditions.checkArgument("EC".equals(key.kty));
+      com.google.common.base.Preconditions.checkArgument("P-256".equals(key.crv));
+
+      BigInteger x = new BigInteger(1, Base64.decodeBase64(key.x));
+      BigInteger y = new BigInteger(1, Base64.decodeBase64(key.y));
+      ECPoint pubPoint = new ECPoint(x, y);
+      AlgorithmParameters parameters = AlgorithmParameters.getInstance("EC");
+      parameters.init(new ECGenParameterSpec("secp256r1"));
+      ECParameterSpec ecParameters = parameters.getParameterSpec(ECParameterSpec.class);
+      ECPublicKeySpec pubSpec = new ECPublicKeySpec(pubPoint, ecParameters);
+      KeyFactory kf = KeyFactory.getInstance("EC");
+      return kf.generatePublic(pubSpec);
+    }
+  }
+
+  /** Custom exception for wrapping all verification errors. */
+  public static class VerificationException extends Exception {
+    public VerificationException(String message) {
+      super(message);
+    }
+
+    public VerificationException(String message, Throwable cause) {
+      super(message, cause);
     }
   }
 }
--- google-oauth-client/src/test/java/com/google/api/client/auth/openidconnect/IdTokenVerifierTest.java
+++ google-oauth-client/src/test/java/com/google/api/client/auth/openidconnect/IdTokenVerifierTest.java
@@ -15,9 +15,19 @@
 package com.google.api.client.auth.openidconnect;
 
 import com.google.api.client.auth.openidconnect.IdToken.Payload;
+import com.google.api.client.auth.openidconnect.IdTokenVerifier.VerificationException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.http.LowLevelHttpResponse;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.webtoken.JsonWebSignature.Header;
+import com.google.api.client.testing.http.MockHttpTransport;
+import com.google.api.client.testing.http.MockLowLevelHttpRequest;
+import com.google.api.client.testing.http.MockLowLevelHttpResponse;
 import com.google.api.client.util.Clock;
 import com.google.api.client.util.Lists;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -38,6 +48,24 @@ public class IdTokenVerifierTest extends TestCase {
   private static final String ISSUER = "issuer.example.com";
   private static final String ISSUER2 = ISSUER + "2";
   private static final String ISSUER3 = ISSUER + "3";
+
+  private static final String ES256_TOKEN =
+      "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im1wZjBEQSJ9.eyJhdWQiOiIvcHJvamVjdHMvNjUyNTYyNzc2Nzk4L2FwcHMvY2xvdWQtc2FtcGxlcy10ZXN0cy1waHAtaWFwIiwiZW1haWwiOiJjaGluZ29yQGdvb2dsZS5jb20iLCJleHAiOjE1ODQwNDc2MTcsImdvb2dsZSI6eyJhY2Nlc3NfbGV2ZWxzIjpbImFjY2Vzc1BvbGljaWVzLzUxODU1MTI4MDkyNC9hY2Nlc3NMZXZlbHMvcmVjZW50U2VjdXJlQ29ubmVjdERhdGEiLCJhY2Nlc3NQb2xpY2llcy81MTg1NTEyODA5MjQvYWNjZXNzTGV2ZWxzL3Rlc3ROb09wIiwiYWNjZXNzUG9saWNpZXMvNTE4NTUxMjgwOTI0L2FjY2Vzc0xldmVscy9ldmFwb3JhdGlvblFhRGF0YUZ1bGx5VHJ1c3RlZCJdfSwiaGQiOiJnb29nbGUuY29tIiwiaWF0IjoxNTg0MDQ3MDE3LCJpc3MiOiJodHRwczovL2Nsb3VkLmdvb2dsZS5jb20vaWFwIiwic3ViIjoiYWNjb3VudHMuZ29vZ2xlLmNvbToxMTIxODE3MTI3NzEyMDE5NzI4OTEifQ.yKNtdFY5EKkRboYNexBdfugzLhC3VuGyFcuFYA8kgpxMqfyxa41zkML68hYKrWu2kOBTUW95UnbGpsIi_u1fiA";
+
+  private static final String FEDERATED_SIGNON_RS256_TOKEN =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6ImY5ZDk3YjRjYWU5MGJjZDc2YWViMjAwMjZmNmI3NzBjYWMyMjE3ODMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2V4YW1wbGUuY29tL3BhdGgiLCJhenAiOiJpbnRlZ3JhdGlvbi10ZXN0c0BjaGluZ29yLXRlc3QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJlbWFpbCI6ImludGVncmF0aW9uLXRlc3RzQGNoaW5nb3ItdGVzdC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE1ODc2Mjk4ODgsImlhdCI6MTU4NzYyNjI4OCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwic3ViIjoiMTA0MDI5MjkyODUzMDk5OTc4MjkzIn0.Pj4KsJh7riU7ZIbPMcHcHWhasWEcbVjGP4yx_5E0iOpeDalTdri97E-o0dSSkuVX2FeBIgGUg_TNNgJ3YY97T737jT5DUYwdv6M51dDlLmmNqlu_P6toGCSRC8-Beu5gGmqS2Y82TmpHH9Vhoh5PsK7_rVHk8U6VrrVVKKTWm_IzTFhqX1oYKPdvfyaNLsXPbCt_NFE0C3DNmFkgVhRJu7LtzQQN-ghaqd3Ga3i6KH222OEI_PU4BUTvEiNOqRGoMlT_YOsyFN3XwqQ6jQGWhhkArL1z3CG2BVQjHTKpgVsRyy_H6WTZiju2Q-XWobgH-UPSZbyymV8-cFT9XKEtZQ";
+  private static final String LEGACY_FEDERATED_SIGNON_CERT_URL =
+      "https://www.googleapis.com/oauth2/v1/certs";
+
+  private static final String SERVICE_ACCOUNT_RS256_TOKEN =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IjJlZjc3YjM4YTFiMDM3MDQ4NzA0MzkxNmFjYmYyN2Q3NGVkZDA4YjEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2V4YW1wbGUuY29tL2F1ZGllbmNlIiwiZXhwIjoxNTg3NjMwNTQzLCJpYXQiOjE1ODc2MjY5NDMsImlzcyI6InNvbWUgaXNzdWVyIiwic3ViIjoic29tZSBzdWJqZWN0In0.gGOQW0qQgs4jGUmCsgRV83RqsJLaEy89-ZOG6p1u0Y26FyY06b6Odgd7xXLsSTiiSnch62dl0Lfi9D0x2ByxvsGOCbovmBl2ZZ0zHr1wpc4N0XS9lMUq5RJQbonDibxXG4nC2zroDfvD0h7i-L8KMXeJb9pYwW7LkmrM_YwYfJnWnZ4bpcsDjojmPeUBlACg7tjjOgBFbyQZvUtaERJwSRlaWibvNjof7eCVfZChE0PwBpZc_cGqSqKXv544L4ttqdCnmONjqrTATXwC4gYxruevkjHfYI5ojcQmXoWDJJ0-_jzfyPE4MFFdCFgzLgnfIOwe5ve0MtquKuv2O0pgvg";
+  private static final String SERVICE_ACCOUNT_CERT_URL =
+      "https://www.googleapis.com/robot/v1/metadata/x509/integration-tests%40chingor-test.iam.gserviceaccount.com";
+
+  private static final List<String> ALL_TOKENS =
+      Arrays.asList(ES256_TOKEN, FEDERATED_SIGNON_RS256_TOKEN, SERVICE_ACCOUNT_RS256_TOKEN);
+
+  static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
   private static IdToken newIdToken(String issuer, String audience) {
     Payload payload = new Payload();
@@ -138,7 +166,7 @@ public class IdTokenVerifierTest extends TestCase {
     assertNull(verifier.getIssuer());
   }
 
-  public void testMissingAudience() {
+  public void testMissingAudience() throws VerificationException {
     IdToken idToken = newIdToken(ISSUER, null);
 
     MyClock clock = new MyClock();
@@ -150,5 +178,124 @@ public class IdTokenVerifierTest extends TestCase {
             .setClock(clock)
             .build();
     assertFalse(verifier.verify(idToken));
+  }
+
+  public void testVerifyEs256TokenPublicKeyMismatch() throws Exception {
+    // Mock HTTP requests
+    HttpTransportFactory httpTransportFactory =
+        new HttpTransportFactory() {
+          @Override
+          public HttpTransport create() {
+            return new MockHttpTransport() {
+              @Override
+              public LowLevelHttpRequest buildRequest(String method, String url)
+                  throws IOException {
+                return new MockLowLevelHttpRequest() {
+                  @Override
+                  public LowLevelHttpResponse execute() throws IOException {
+                    MockLowLevelHttpResponse response = new MockLowLevelHttpResponse();
+                    response.setStatusCode(200);
+                    response.setContentType("application/json");
+                    response.setContent("");
+                    return response;
+                  }
+                };
+              }
+            };
+          }
+        };
+    MyClock clock = new MyClock();
+    clock.timeMillis = 1582704000000L;
+    IdTokenVerifier tokenVerifier =
+        new IdTokenVerifier.Builder()
+            .setClock(clock)
+            .setHttpTransportFactory(httpTransportFactory)
+            .build();
+
+    try {
+      tokenVerifier.verify(IdToken.parse(JSON_FACTORY, ES256_TOKEN));
+    } catch (VerificationException ex) {
+      assertTrue(ex.getMessage().contains("Error fetching PublicKey"));
+    }
+    throw new Exception("Should have failed verification");
+  }
+
+  // @Test
+  // void verifyEs256Token() throws TokenVerifier.VerificationException, IOException {
+  //   HttpTransportFactory httpTransportFactory =
+  //       mockTransport(
+  //           "https://www.gstatic.com/iap/verify/public_key-jwk",
+  //           readResourceAsString("iap_keys.json"));
+  //   TokenVerifier tokenVerifier =
+  //       TokenVerifier.newBuilder()
+  //           .setClock(FIXED_CLOCK)
+  //           .setHttpTransportFactory(httpTransportFactory)
+  //           .build();
+  //   assertNotNull(tokenVerifier.verify(ES256_TOKEN));
+  // }
+  //
+  // @Test
+  // void verifyRs256Token() throws TokenVerifier.VerificationException, IOException {
+  //   HttpTransportFactory httpTransportFactory =
+  //       mockTransport(
+  //           "https://www.googleapis.com/oauth2/v3/certs",
+  //           readResourceAsString("federated_keys.json"));
+  //   TokenVerifier tokenVerifier =
+  //       TokenVerifier.newBuilder()
+  //           .setClock(FIXED_CLOCK)
+  //           .setHttpTransportFactory(httpTransportFactory)
+  //           .build();
+  //   assertNotNull(tokenVerifier.verify(FEDERATED_SIGNON_RS256_TOKEN));
+  // }
+  //
+  // @Test
+  // void verifyRs256TokenWithLegacyCertificateUrlFormat()
+  //     throws TokenVerifier.VerificationException, IOException {
+  //   HttpTransportFactory httpTransportFactory =
+  //       mockTransport(
+  //           LEGACY_FEDERATED_SIGNON_CERT_URL, readResourceAsString("legacy_federated_keys.json"));
+  //   TokenVerifier tokenVerifier =
+  //       TokenVerifier.newBuilder()
+  //           .setCertificatesLocation(LEGACY_FEDERATED_SIGNON_CERT_URL)
+  //           .setClock(FIXED_CLOCK)
+  //           .setHttpTransportFactory(httpTransportFactory)
+  //           .build();
+  //   assertNotNull(tokenVerifier.verify(FEDERATED_SIGNON_RS256_TOKEN));
+  // }
+  //
+  // @Test
+  // void verifyServiceAccountRs256Token() throws TokenVerifier.VerificationException, IOException {
+  //   TokenVerifier tokenVerifier =
+  //       TokenVerifier.newBuilder()
+  //           .setClock(FIXED_CLOCK)
+  //           .setCertificatesLocation(SERVICE_ACCOUNT_CERT_URL)
+  //           .build();
+  //   assertNotNull(tokenVerifier.verify(SERVICE_ACCOUNT_RS256_TOKEN));
+  // }
+
+  static HttpTransportFactory mockTransport(String url, String certificates) {
+    final String certificatesContent = certificates;
+    final String certificatesUrl = url;
+    return new HttpTransportFactory() {
+      @Override
+      public HttpTransport create() {
+        return new MockHttpTransport() {
+          @Override
+          public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
+            assertEquals(certificatesUrl, url);
+            return new MockLowLevelHttpRequest() {
+              @Override
+              public LowLevelHttpResponse execute() throws IOException {
+                MockLowLevelHttpResponse response = new MockLowLevelHttpResponse();
+                response.setStatusCode(200);
+                response.setContentType("application/json");
+                response.setContent(certificatesContent);
+                return response;
+              }
+            };
+          }
+        };
+      }
+    };
   }
 }
